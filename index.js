0796f59bd8a9eed2cdb9a33c2fa8a2a63b06c7ac
asim sheikh <asmsheikh123@gmail.com>
2022-02-27 19:29:18 +0530

    cors code changes plus added http before url

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 let express=require('express');
 let fs=require('fs');
 let app=express();
-let cors=require('cors')
+let cors=require('cors');
 var port =(process.env.PORT || '3000');
 let products=[]
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 app.use(express.json())
 
 app.get('/',(req,res)=>{
